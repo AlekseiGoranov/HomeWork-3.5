4629b6a851cb530c58657f0dd80fad2405473746
Aleksei <goranov93@mail.ru>
2021-04-08 11:58:50 +0300

    Initial Commit

---
+++ HomeWork 3.5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeWork 3.5
+//
+//  Created by Aleksei Goranov on 08.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HomeWork 3.5/HomeWork_3_5App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork_3_5App.swift
+//  HomeWork 3.5
+//
+//  Created by Aleksei Goranov on 08.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork_3_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
